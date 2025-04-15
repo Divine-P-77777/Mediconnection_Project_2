d9555417144a4dc6fe11fcba83350217d2bd0419
Deepak432-ai <deepak_2312res239@iitp.ac.in>
2025-04-15 23:28:41 +0530

    change

--- src/utils/supabase/client.js
+++ src/utils/supabase/client.js
@@ -1,13 +1,8 @@
 import { createBrowserClient } from "@supabase/ssr";
 
-let supabase = null;
+const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
 
-export function createClient() {
-  if (!supabase) {
-    supabase = createBrowserClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-    );
-  }
-  return supabase;
-}
+export default supabase;
