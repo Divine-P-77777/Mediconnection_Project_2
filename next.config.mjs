c083270b9ef2bc130f0b9e9c468739f207eeb120
Deepak432-ai <deepak_2312res239@iitp.ac.in>
2025-04-16 00:54:21 +0530

    change

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['res.cloudinary.com'], // Add your Cloudinary domain
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'crueywamzqfundowraoc.supabase.co',
+          pathname: '/storage/v1/object/public/**',
+        },
+      ],
+    },
+    experimental: {
+      serverActions: {
+        allowedOrigins: ['localhost:3000', 'mediconnection.vercel.app'],
+      },
+    },
+    // Enable React strict mode for better development experience
+    reactStrictMode: true,
+    // Configure webpack if needed
+    webpack: (config) => {
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
